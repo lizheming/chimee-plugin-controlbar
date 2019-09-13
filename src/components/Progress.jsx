291dcaacab8167bd65f8a661e9c745bbe6ba5882
lizheming <i@imnerd.org>
2019-09-13 10:56:10 +0800

    use useBuffer and useDragX reconstruct progress plugin

--- src/components/Progress.jsx
+++ src/components/Progress.jsx
@@ -1,70 +1,87 @@
-import React, { useContext, useRef, useState, useEffect } from 'react';
+import React, { useContext, useRef, useState, useEffect, useCallback } from 'react';
 import cls from 'classnames';
 import formatTime from '../utils/duration';
 import Context from '../Context';
 
-export default function () {
+function useBuffer(ctx) {
+  const [buffer, setBuffer] = useState(0);
+
+  const onProgress = useCallback(() => {
+    try {
+      const buffer = ctx.buffered.end(0);
+      setBuffer(buffer);
+    } catch (e) {
+      console.log(e);
+    }
+  }, []);
+
+  useEffect(() => {
+    ctx.$on('progress', onProgress);
+    return () => ctx.$off('progress', onProgress);
+  }, []);
+
+  return buffer / ctx.duration || 0;
+}
+
+function useDragX(dom) {
   const ctx = useContext(Context);
-  const wrap = useRef(null);
-  const [startX, upX] = useState(null);
-  const [startTime, upTime] = useState(null);
+  const [percent, upPer] = useState(0);
   const [active, upStatus] = useState(false);
   const [currentTime, setCurrentTime] = useState(0);
-  const [buffer, setBuffer] = useState(0);
-  useEffect(() => {
-    function onProgress() {
-      try {
-        const buffer = ctx.buffered.end(0);
-        setBuffer(buffer);
-      } catch (e) {
-        console.log(e);
-      }
-    }
-    function onTimeUpdate() {
-      setCurrentTime(ctx.currentTime);
-    }
-    ctx.$on('progress', onProgress);
-    ctx.$on('timeupdate', onTimeUpdate);
-    return () => {
-      // ctx.$off('progress', onProgress);
-      // ctx.$off('timeupdate', onTimeUpdate);
-    };
-  }, []);
-  function draging(e) {
-    const endX = e.clientX;
-    const offsetWidth = wrap.current.offsetWidth;
-    const dragTime = (endX - startX) / offsetWidth * ctx.duration;
-    const dragAfterTime = +(startTime + dragTime).toFixed(2);
-    const currentTime = Math.min(ctx.duration, Math.max(0, dragAfterTime));
-    setCurrentTime(currentTime);
-  }
-  function dragEnd() {
-    upX(null);
-    upTime(null);
-    ctx.currentTime = currentTime;
-    setCurrentTime(undefined);
+
+  const draging = useCallback(e => {
+    const { width, left } = dom.getBoundingClientRect();
+    upPer((e.pageX - left) / width);
+  }, [dom]);
+
+  const dragEnd = useCallback(e => {
+    const { width, left } = dom.getBoundingClientRect();
+    ctx.currentTime = (e.pageX - left) / width * ctx.duration;
+    upPer(0);
     upStatus(false);
     window.removeEventListener('mousemove', draging);
     window.removeEventListener('mouseup', dragEnd);
     window.removeEventListener('contextmenu', dragEnd);
-  }
-  function onMouseDown(e) {
-    if (isNaN(ctx.duration)) {
-      console.error('isNaN(duration) === true');
-      return;
+  }, [dom]);
+
+  const onTimeUpdate = useCallback(() => setCurrentTime(ctx.currentTime), []);
+  useEffect(() => ctx.$on('timeupdate', onTimeUpdate), []);
+
+  return {
+    active,
+    currentTime: active ? percent * ctx.duration : currentTime,
+    onMouseDown: e => {
+      if (isNaN(ctx.duration)) {
+        console.error('isNaN(duration) === true');
+        return;
+      }
+
+      if (!dom) {
+        return;
+      }
+
+      draging(e);
+      upStatus(true);
+      window.addEventListener('mousemove', draging);
+      window.addEventListener('mouseup', dragEnd);
+      window.addEventListener('contextmenu', dragEnd);
     }
-    const pageX = wrap.current.getBoundingClientRect().x;
-    const offsetWidth = wrap.current.offsetWidth;
-    const time = (e.pageX - pageX) / offsetWidth * ctx.duration;
-    upX(e.clientX);
-    upTime(time);
-    upStatus(true);
-    window.addEventListener('mousemove', draging);
-    window.addEventListener('mouseup', dragEnd);
-    window.addEventListener('contextmenu', dragEnd);
   }
+}
+
+export default function () {
+  const ctx = useContext(Context);
+  const wrap = useRef(null);
+  const buffer = useBuffer(ctx);
+  const {
+    active,
+    currentTime,
+    onMouseDown
+  } = useDragX(wrap.current);
+
   const timePer = currentTime ? currentTime / ctx.duration : 0;
   const offset = (timePer * 100) + '%';
+
   return (
     <div
       className={cls('chimee-controlbar__progress', {
@@ -75,7 +92,7 @@ export default function () {
     >
       <div
         className="chimee-controlbar__progress__buff"
-        style={{ width: (buffer / ctx.duration * 100) + '%' }}
+        style={{ width: (buffer * 100) + '%' }}
       />
       <div
         className="chimee-controlbar__progress__play"
