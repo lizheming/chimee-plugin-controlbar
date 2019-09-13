dfb766a3d4f9e8cf79c4ca0318d868ddbed46a2c
lizheming <i@imnerd.org>
2019-09-13 18:18:52 +0800

    add hover hide logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext, useEffect, useState, useCallback } from 'react';
 import Progress from './components/Progress';
 import Play from './components/Play';
 import Time from './components/Time';
@@ -7,9 +7,46 @@ import PlaybackRate from './components/PlaybackRate';
 import Volume from './components/Volume';
 import FullScreen from './components/FullScreen';
 
+import Context from './Context';
+
 import './style.less';
 
+function useHoverhide() {
+  const ctx = useContext(Context);
+  const [status, upStatus] = useState(false);
+
+  const hide = useCallback(() => upStatus(false), []);
+  const show = useCallback(() => {
+    upStatus(true);
+    if (ctx.autoHideId) {
+      clearTimeout(ctx.autoHideId);
+    }
+    ctx.autoHideId = setTimeout(hide, 5000);
+  }, []);
+
+  useEffect(() => {
+    ctx.$on('mouseenter', show, { target: 'container' });
+    ctx.$on('mousemove', show, { target: 'container' });
+    ctx.$on('click', show, { target: 'container' });
+    ctx.$on('mouseleave', hide, { target: 'container' });
+    return () => {
+      ctx.$off('mouseenter', show, { target: 'container' });
+      ctx.$off('mousemove', show, { target: 'container' });
+      ctx.$off('click', show, { target: 'container' });
+      ctx.$off('mouseleave', hide, { target: 'container' });
+    }
+  }, []);
+
+  return status;
+};
+
+
 export default function () {
+  const status = useHoverhide();
+  if (!status) {
+    return null;
+  }
+
   return (
     <>
       <Progress />
